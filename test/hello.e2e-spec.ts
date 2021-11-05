177a2b9004d5787baa27be3bde841678dba4abf2
Jon Ceanfaglione <jonc@jons-mbp.lan>
2021-11-05 17:05:08 -0400

    adding e3e test

---
+++ test/hello.e2e-spec.ts
@@ -0,0 +1,28 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module'
+
+describe('HelLoModule (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        operationName: null,
+        variables: {},
+        query: '{getProducts{id, price}}',
+      })
+      .expect(200);
+  });
+});
